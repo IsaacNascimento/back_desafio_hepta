1a66a1c13db8b82cf3ee0ffaa71c18572c7e9b5f
ADIsaacNascimento <104455835+ADIsaacNascimento@users.noreply.github.com>
2022-09-14 02:08:16 -0300

    Bug fix contato

--- base/serializers.py
+++ base/serializers.py
@@ -1,12 +1,37 @@
 from rest_framework import serializers
 from .models import Tarefa, Contato
 
-class TarefaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tarefa
-        fields = '__all__'
+class RelatedFieldAlternative(serializers.PrimaryKeyRelatedField):
+    def __init__(self, **kwargs):
+        self.serializer = kwargs.pop('serializer', None)
+        if self.serializer is not None and not issubclass(self.serializer, serializers.Serializer):
+            raise TypeError('"serializer" is not a valid serializer class')
+
+        super().__init__(**kwargs)
+
+    def use_pk_only_optimization(self):
+        return False if self.serializer else True
+
+    def to_representation(self, instance):
+        if self.serializer:
+            return self.serializer(instance, context=self.context).data
+        return super().to_representation(instance)
+
 
 class ContatoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contato
+        fields = '__all__'        
+
+class TarefaSerializer(serializers.ModelSerializer):
+    contato = RelatedFieldAlternative(queryset=Contato.objects.all(), serializer=ContatoSerializer)
+
+    class Meta:
+        model = Tarefa
         fields = '__all__'
+
+    def to_representation(self, instance):
+        response = super().to_representation(instance)
+        response['contato'] = ContatoSerializer(instance.contato).data
+        return response    
+
